8f4cdda17ac868f96231737324a04ef82eb29fb0
c0mpileErr0r <ariellin0801@gmail.com>
2026-05-11 11:06:28 +0800

    Update score.py

--- score.py
+++ score.py
@@ -9,6 +9,7 @@ Original file is located at
 
 score = input().split()
 failnum=0
+<<<<<<< Updated upstream
 sum = 0
 for i in score:
   sum += int(i)
@@ -18,3 +19,9 @@ l = len(score)
 avg = sum / l
 print("不及格人數:",failnum)
 print("平均數:", avg)
+=======
+for i in score:
+  if int(i) < 60:
+    failnum = failnum+1
+print("不及格人數:",failnum)
+>>>>>>> Stashed changes
