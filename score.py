041b2c539de4bbeccb3a412f37bdae6ed2667b88
Mika0630 <ericmika0630@gmail.com>
2026-05-11 11:19:17 +0800

    Update score.py

--- score.py
+++ score.py
@@ -12,4 +12,11 @@ failnum=0
 for i in score:
   if int(i) < 60:
     failnum = failnum+1
-print("不及格人數:",failnum)
+print("不及格人數:",failnum)print("不及格人數:",failnum)
+
+scores = []
+for i in score:
+  scores.append(int(i))
+
+print("最高分:", max(scores))
+print("最低分:", min(scores))
